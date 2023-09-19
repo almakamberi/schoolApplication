eaa0151adb5d617f8cfa52a3e55c03f8e3af63b2
almakamberi <45633277+almakamberi@users.noreply.github.com>
2023-09-19 21:41:49 +0200

    Updated Handlers & functions on main.go

--- main.go
+++ main.go
@@ -33,8 +33,8 @@ func main() {
 	r.HandleFunc("/students", handler.GetStudents).Methods("GET")
 	r.HandleFunc("/students/{id:[0-9]+}", handler.UpdateStudent).Methods("PUT")
 	r.HandleFunc("/students/{id:[0-9]+}", handler.DeleteStudent).Methods("DELETE")
-	r.HandleFunc("/class", handlerC.CreateClass).Methods("POST")
-	r.HandleFunc("/class", handlerC.GetClasses).Methods("GET")
+	r.HandleFunc("/classes", handlerC.CreateClass).Methods("POST")
+	r.HandleFunc("/classes", handlerC.GetClasses).Methods("GET")
 	r.HandleFunc("/classes/{id:[0-9]+}", handlerC.UpdateClass).Methods("PUT")
 	r.HandleFunc("/classes/{id:[0-9]+}", handlerC.DeleteClass).Methods("DELETE")
 
